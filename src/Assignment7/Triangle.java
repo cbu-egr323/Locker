cda5381d87a095b50488bacbf9c70b32e9b0838f
Alicia Locker <wiisha.locker@gmail.com>
2019-03-28 20:32:37 -0700

    Initial Commit of Final Program Completed

--- src/Assignment7/Triangle.java
+++ src/Assignment7/Triangle.java
@@ -1,44 +1,56 @@
 package Assignment7;
 
-public class Triangle
-{
+public class Triangle {
 
     private boolean isEquilateral = false;
     private boolean isScalene = false;
     private boolean isIsosceles = false;
-    //private boolean invalidTri = false;
     private double s1;
     private double s2;
     private double s3;
 
     //******************************************************************
-    public Triangle(double sideA, double sideB, double sideC){
+    public Triangle(double sideA, double sideB, double sideC) {
         this.s1 = sideA;
         this.s2 = sideB;
         this.s3 = sideC;
     }
     //******************************************************************
+
+    public double getS1() {
+        return s1;
+    }
+
+    public double getS2() {
+        return s2;
+    }
+
+    public double getS3() {
+        return s3;
+    }
+
+    //******************************************************************
     public boolean isEquilateral() {
-        if (s1 == s2)  {
+        if ((s1 == s2) && (s1 == s3) && (s2 == s3)) {
             isEquilateral = true;
             System.out.println("The triangle is EQUILATERAL!");
         }
         return isEquilateral;
     }
+
     //******************************************************************
     public boolean isScalene() {
-        if ( (s1 != s2)  ) {
+        if ((s1 != s2) && (s1 != s3) && (s2 != s3)) {
             isScalene = true;
             System.out.println("The triangle is SCALENE!");
         }
         return isScalene;
     }
+
     //******************************************************************
     public boolean isIsosceles() {
-        if (
-                ( (s1 == s2) || (s1 != s3) ) ||
-                ( (s1 == s3) || (s1 != s2) ) ||
-                ( (s2 == s3) || (s2 != s1) ) ) {
+        if (((s1 == s2) || (s1 == s3) || (s2 == s3)) &&
+                ((s1 != s3) || (s1 != s2) || (s2 != s1))) {
             isIsosceles = true;
             System.out.println("The triangle is ISOSCELES!");
         }
